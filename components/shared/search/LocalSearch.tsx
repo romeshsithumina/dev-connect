c70992a3295a5520ce3fe5f85acad68cd5df6bf2
Romesh Senarathna <romeshsithumina@gmail.com>
2023-10-24 22:49:15 +0530

    feat(ui): Implement LocalSearchbox and Home Filters
    * LocalSearchbox is used to search questions in the Home
    * Filters are used to filter questions in the Home

---
+++ components/shared/search/LocalSearch.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+
+interface LocalSearchProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({
+  route,
+  iconPosition,
+  imgSrc,
+  placeholder,
+  otherClasses,
+}: LocalSearchProps) => {
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          alt="search question"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value=""
+        onChange={() => {}}
+        className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+      />
+
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          alt="search question"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+export default LocalSearch;

bb6e7c1e926cd82d9897f35d2b8609a4386ed09f
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-21 23:22:41 +0530

    feat(ui): Implement HTML parsing for question content
    * Implemented the parsing of code blocks within question content to render them as HTML.
    * Added syntax highlighting.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 import "./globals.css";
+import "../styles/prism.css";
+
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";

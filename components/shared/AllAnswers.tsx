b5d140742825e4d6435c374766ec7316e4411570
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-26 23:21:52 +0530

    feat(server-actions): Implement answer upvote/downvote server actions

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -61,7 +61,17 @@ const AllAnswers = async ({
                   </div>
                 </Link>
 
-                <div className="flex justify-end">{/* <Votes /> */}</div>
+                <div className="flex justify-end">
+                  <Votes
+                    type="Answer"
+                    itemId={JSON.stringify(answer._id)}
+                    userId={JSON.stringify(userId)}
+                    upvotes={answer.upvotes.length}
+                    hasupVoted={answer.upvotes.includes(userId)}
+                    downvotes={answer.downvotes.length}
+                    hasdownVoted={answer.downvotes.includes(userId)}
+                  />
+                </div>
               </div>
             </div>
             <ParseHTML data={answer.content} />

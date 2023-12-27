9a964e7aebe9618cfdca213c3f942b1787c9dc0d
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-27 19:44:12 +0530

    feat(server-actions): Implement Question Views server action
    * Introduces a new Question views server action to update no. of question views on the server side.
    * Will be used for the question recommendations in the future.

---
+++ lib/actions/interaction.action.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import { ViewQuestionParams } from "./shared.types";
+import Interaction from "@/database/interaction.model";
+
+export async function viewQuestion(params: ViewQuestionParams) {
+  try {
+    connectToDatabase();
+
+    const { userId, questionId } = params;
+
+    // Update the view count for the question
+    await Question.findByIdAndUpdate(questionId, { $inc: { views: 1 } });
+
+    if (userId) {
+      const existingInteraction = await Interaction.findOne({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+
+      if (existingInteraction) {
+        console.log("User has already viewed");
+      }
+
+      // Create interaction
+      await Interaction.create({
+        user: userId,
+        action: "view",
+        question: questionId,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function name(params: GetAllUsersParams) {
+//   try {
+//     connectToDatabase();
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }

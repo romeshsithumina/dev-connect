dbacb368131b9846330993af659ee1a6b04155b8
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-29 18:41:07 +0530

    refactor(server-actions): Remove unused parameters

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -145,7 +145,8 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    // const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const { clerkId, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }

ff09a8f2a84dfe9d90828cb7cd0fac5a21da5c69
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-29 22:42:25 +0530

    feat(server-actions): Implement getUserInfo server action
    * Add new getUserInfo action to retrieve user-specific data (total
    questions and answers) to be used in the user profile.

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import Question from "@/database/question.model";
-import { FilterQuery } from "mongoose";
+import Tag from "@/database/tag.model";
 import User from "@/database/user.model";
+import { FilterQuery } from "mongoose";
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../mongoose";
 import {
@@ -10,10 +11,11 @@ import {
   DeleteUserParams,
   GetAllUsersParams,
   GetSavedQuestionsParams,
+  GetUserByIdParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
-import Tag from "@/database/tag.model";
+import Answer from "@/database/answer.model";
 
 export async function getUserById(params: any) {
   try {
@@ -169,6 +171,30 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
     const savedQuestions = user.saved;
 
     return { questions: savedQuestions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getUserInfo(params: GetUserByIdParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) throw new Error("User not found");
+
+    const totalQuestions = await Question.countDocuments({ author: user._id });
+    const totalAnswers = await Answer.countDocuments({ author: user._id });
+
+    return {
+      user,
+      totalQuestions,
+      totalAnswers,
+    };
   } catch (error) {
     console.log(error);
     throw error;

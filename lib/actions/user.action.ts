3bc81799b1dacc2624e515c0173317d8b2fbdab4
Romesh Senarathna <romeshsithumina@gmail.com>
2023-12-30 13:01:41 +0530

    feat(server-actions): Implement getUserQuestions server action
    * Add new getUserQuestions action to retrieve all the questions of a user sorted in descending order of views and upvotes.

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -12,6 +12,7 @@ import {
   GetAllUsersParams,
   GetSavedQuestionsParams,
   GetUserByIdParams,
+  GetUserStatsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
@@ -194,6 +195,29 @@ export async function getUserInfo(params: GetUserByIdParams) {
       user,
       totalQuestions,
       totalAnswers,
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getUserQuestions(params: GetUserStatsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+
+    const totalQuestions = await Question.countDocuments({ author: userId });
+
+    const userQuestions = await Question.find({ author: userId })
+      .sort({ views: -1, upvotes: -1 })
+      .populate("tags", "_id name")
+      .populate("author", "_id clerkId name picture");
+
+    return {
+      totalQuestions,
+      questions: userQuestions,
     };
   } catch (error) {
     console.log(error);

d0ea28c3f5c3c310f9961746662c038deb1bea83
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-01 20:18:34 +0530

    feat(ui): Implement dynamic fetching of popular questions and tags in Home page

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -1,36 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "../RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getPopularTags } from "@/lib/actions/tag.action";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-  },
-  {
-    _id: "2",
-    title: "Redux Toolkit Not Updating State as Expected",
-  },
-  {
-    _id: "3",
-    title: "Async/Await Function Not Handling Errors Properly",
-  },
-  {
-    _id: "4",
-    title: "How do I use express as a custom server in NextJS?",
-  },
-];
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getPopularTags();
 
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "3", name: "python", totalQuestions: 8 },
-  { _id: "3", name: "react", totalQuestions: 5 },
-  { _id: "4", name: "tailwindcss", totalQuestions: 2 },
-  { _id: "5", name: "nextjs", totalQuestions: 6 },
-];
-
-const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -39,7 +16,7 @@ const RightSidebar = () => {
           {hotQuestions.map((question) => (
             <Link
               key={question._id}
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">

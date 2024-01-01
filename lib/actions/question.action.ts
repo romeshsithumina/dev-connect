a1a75b6ecd628d868a56bb14bc3695074982948a
Romesh Senarathna <romeshsithumina@gmail.com>
2024-01-01 19:54:05 +0530

    feat(server-actions): Implement getHotQuestions server action
    * Add new getHotQuestions action to get the 5 most voted and viewed questions.

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -207,6 +207,21 @@ export async function editQuestion(params: EditQuestionParams) {
   }
 }
 
+export async function getHotQuestions() {
+  try {
+    connectToDatabase();
+
+    const hotQuestions = await Question.find({})
+      .sort({ upvotes: -1, views: -1 })
+      .limit(5);
+
+    return hotQuestions;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 // export async function name(params: GetAllUsersParams) {
 //   try {
 //     connectToDatabase();

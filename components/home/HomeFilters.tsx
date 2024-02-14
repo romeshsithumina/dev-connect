54add365019b5036bf93b0af65a4336d07932990
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-14 23:26:08 +0530

    feat: implement filter state management in HomeFilters component

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -2,21 +2,51 @@
 
 import { HomePageFilters } from "@/constants/filters";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  const active = "newest";
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const [active, setActive] = useState("");
+
+  const handleTypeClick = (item: string) => {
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+
+      router.push(newUrl, { scroll: false });
+    }
+  };
 
   return (
     <div className="mt-10 hidden flex-wrap gap-3 md:flex">
       {HomePageFilters.map((filter) => (
         <Button
           key={filter.value}
-          onClick={() => {}}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === filter.value
               ? "bg-primary-100 text-primary-500"
               : "bg-light-800 text-light-500"
           }`}
+          onClick={() => {
+            handleTypeClick(filter.value);
+          }}
         >
           {filter.name}
         </Button>

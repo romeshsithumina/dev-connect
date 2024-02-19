b04306a683d6458cc27731d26cdd21ea6e2a5e8d
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-19 22:56:00 +0530

    feat(search): implement global search query state management
    * Added a 500ms debounce to update the URL with the global search query.

---
+++ components/shared/search/GlobalResult.tsx
@@ -0,0 +1,4 @@
+const GlobalResult = () => {
+  return <div>GlobalResult</div>;
+};
+export default GlobalResult;
--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,49 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import GlobalResult from "./GlobalResult";
 
 const GlobalSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("global");
+
+  const [search, setSearch] = useState(query || "");
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "global",
+          value: search,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (query) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["global", "type"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+
+      return () => {
+        clearTimeout(delayDebounceFn);
+      };
+    }, 500);
+  }, [search, router, pathname, searchParams, query]);
+
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
@@ -15,10 +57,18 @@ const GlobalSearch = () => {
 
         <Input
           type="text"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+
+            if (!isOpen) setIsOpen(true);
+            if (e.target.value === "" && isOpen) setIsOpen(false);
+          }}
           placeholder="Seach globally"
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+          className="text-dark400_light700 paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
+      {isOpen && <GlobalResult />}
     </div>
   );
 };

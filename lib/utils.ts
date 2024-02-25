87de66907ea5248417cbabde23a53a513778318a
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-25 23:54:28 +0530

    feat(utils): implement assignBadges utility function
    * This function takes criteria for different badge types, evaluates them based on predefined badge criteria, and returns the counts for each badge level (GOLD, SILVER, BRONZE).

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
+import { BADGE_CRITERIA } from "@/constants";
+import { BadgeCounts } from "@/types";
+import { clsx, type ClassValue } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
 
@@ -106,4 +108,34 @@ export const removeKeysFromQuery = ({
       skipNull: true,
     }
   );
+};
+
+interface BadgeParam {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA;
+    count: number;
+  }[];
+}
+
+export const assignBadges = (params: BadgeParam) => {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  };
+
+  const { criteria } = params;
+
+  criteria.forEach((item) => {
+    const { type, count } = item;
+    const badgeLevels: any = BADGE_CRITERIA[type];
+
+    Object.keys(badgeLevels).forEach((level: any) => {
+      if (count >= badgeLevels[level]) {
+        badgeCounts[level as keyof BadgeCounts] += 1;
+      }
+    });
+  });
+
+  return badgeCounts;
 };

3e768d8919651ef1d1665a847001f933236052af
Romesh Senarathna <romeshsithumina@gmail.com>
2024-02-27 21:55:02 +0530

    feat(ui): add functionality to generate AI answer in Answer component
    * Implemented the generateAiAnswer function to send a request to the /api/chatgpt endpoint for generating an AI answer.

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -28,6 +28,7 @@ interface AnswerProps {
 const Answer = ({ question, questionId, authorId }: AnswerProps) => {
   const pathname = usePathname();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmittingAi, setIsSubmittingAi] = useState(false);
   const { mode } = useTheme();
   const editorRef = useRef(null);
 
@@ -63,6 +64,37 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
     }
   };
 
+  const generateAiAnswer = async () => {
+    if (!authorId) return;
+
+    setIsSubmittingAi(true);
+
+    try {
+      const response: Response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            question,
+          }),
+        }
+      );
+      const aiAnswer = await response.json();
+
+      const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedAnswer);
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsSubmittingAi(false);
+    }
+  };
+
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -72,7 +104,7 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
 
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-          onClick={() => {}}
+          onClick={generateAiAnswer}
         >
           <Image
             src="/assets/icons/stars.svg"
@@ -81,7 +113,7 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
             height={12}
             className="object-contain"
           />
-          Generate AI Answer
+          {isSubmittingAi ? <>Generating...</> : <>Generate AI Answer </>}
         </Button>
       </div>
 
